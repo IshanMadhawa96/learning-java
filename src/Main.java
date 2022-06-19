3eb354bec54f6e962bb83f1fdb5cef30f44b86f5
Ishan Madhawa <ishan@ninodigitalhouse.com>
2022-06-19 15:47:49 +0530

    java datatype and variables strings

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,15 @@ public class Main {
         char studentFirstInitial = 'K';
         char studentLastInitial = 'H';
         boolean hasPerfectAttendance = true;
+        //string is array of char
+        String studentName = "Ishan Madhawa";
+
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentName);
     }
 }
