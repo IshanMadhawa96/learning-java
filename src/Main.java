24a3c25e390558acca2ff5b4283f8184ce3b4bae
Ishan Madhawa <ishan@ninodigitalhouse.com>
2022-06-19 16:47:40 +0530

    basic string operation added

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         // data type,variable name, = , assgin
-        int studentAge = 15;
+     /* int studentAge = 15;
         double studentGPA = 3.45;
         char studentFirstInitial = 'K';
         char studentLastInitial = 'H';
-        boolean hasPerfectAttendance = true;
+        boolean hasPerfectAttendance = true;*/
+        /* System.out.println(studentAge);
+       System.out.println(studentGPA);
+       System.out.println(studentLastInitial);*
+       System.out.println(hasPerfectAttendance);*/
+        double studentGPA = 3.45;
         //string is array of char
-        String studentName = "Ishan Madhawa";
+        String studentFirstName = "Ishan";
+        String studentLastName = "Madhawa";
+        //acessing string array index
+        char studentFirstInitial = studentFirstName.charAt(0);
 
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
-        System.out.println(studentName);
+        System.out.println("Student Full Name:-"+studentFirstName+" "+studentLastName+"has a GPA of "+studentGPA);
+        System.out.println("Student First Initial:-"+studentFirstInitial);
+        System.out.println("Student Last Initial:-"+studentLastName.charAt(0));
     }
 }
