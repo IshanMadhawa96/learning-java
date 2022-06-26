6fa0148a1a1b07c41ce7c18004404cc4b2ee8352
Ishan Madhawa <ishan@ninodigitalhouse.com>
2022-06-26 23:11:45 +0530

    add basic loop

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Pick a number between 1 and 10");
-        Scanner scanner = new Scanner(System.in);
-        int inputNum = scanner.nextInt();
-        if(inputNum<5){
-           System.out.println("Enjoy good luck brings you!");
-        }else{
-            System.out.println("Your shoe selection will make you very happy today");
+        Scanner input = new Scanner(System.in);
+        boolean isOnRepeat = true;
+        while(isOnRepeat){
+            System.out.println("Playing Current Song");
+            System.out.println("Would you like to take this song off of repeat?If so,answer yes");
+            String userInput=input.next();
+            if(userInput.equals("yes")){
+               isOnRepeat = false;
+            }
         }
-
+        System.out.println("Playing next song");
     }
 }
